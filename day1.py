7690fd1c0d7ca0e1cb5e31f5fe05dc2ab72544ff
shahab <shahaab.kabiri@gmail.com>
2024-08-17 07:19:34 -0700

    first day

--- day1.py
+++ day1.py
@@ -1,1 +1,4 @@
-print("hello")
+print("welcom band name")
+city_live = input("where are you from: ")
+pet_name = input("what is your pet name: ")
+print("your band name" + city_live + " " + pet_name)
